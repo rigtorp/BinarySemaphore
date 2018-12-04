f5ba21219f7bea118d4dd3fc0aea1c6c4870a4ee
Erik Rigtorp <erik@rigtorp.se>
2018-12-04 16:18:14 -0600

    Fix for uaddr2 argument not forwarded

--- BinarySemaphore.h
+++ BinarySemaphore.h
@@ -33,7 +33,7 @@ SOFTWARE.
 namespace {
 static int futex(int *uaddr, int futex_op, int val,
                  const struct timespec *timeout, int *uaddr2, int val3) {
-  return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr, val3);
+  return syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3);
 }
 }
 
